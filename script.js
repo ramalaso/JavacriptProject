e67086df61b405c802d73f80f60f11fee9e103fb
ramalaso <osalamar@gmail.com>
2019-03-01 18:24:48 -0400

    Javascript project

---
+++ script.js
@@ -0,0 +1,28 @@
+/* //variables 
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job); */
+
+//Variable naming rules
+
+/* Multi line comments
+while we are between this two signs */
+
+/* Variable mutation and type coercion */
+
+var firstName = 'Raul';
+var age = 41;
+
+console.log(firstName + ' is ' + age + ' years old.');
+
